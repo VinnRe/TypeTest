225d7233dce6f7db95f0cdf33737b5b168821d68
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-01-10 23:16:34 +0800

    Fix var naming

--- src/gui/home.py
+++ src/gui/home.py
@@ -8,10 +8,10 @@ class Home(CTk.CTkFrame):
         self.label = CTk.CTkLabel(self, text="Home", font=("Roboto", 24))
         self.label.pack(padx=20, pady=20)
 
-        self.button = CTk.CTkButton(self, text="Start Typing!", command=self.switchPage, 
+        self.button = CTk.CTkButton(self, text="Start Typing!", command=self.switch_page, 
                                      fg_color="#4158D0", border_color="#FFCC70", hover_color="#C850C0",
                                      corner_radius=20, font=("Roboto", 24))
         self.button.pack(padx=20, pady=20)
 
-    def switchPage(self):
-        self.master.showFrame(typePage.TypePage)
+    def switch_page(self):
+        self.master.show_frame(typePage.TypePage)
